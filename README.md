8dcc9a972dc720a9b4cfed980b168233860821d1
Frank Guan <fguan@dangermind.com>
2019-03-01 00:41:55 -0800

    Enhancements

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # network-scan
 
 Bash script that performs various network scans and stores results in 
-text file of user's choice
-
+flat file
--- scan.sh
+++ scan.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    # TODO: print usage
-    echo 'Error: Must have 1 argument representing IP address'
+# Bash script for network scanning
+# Frank Guan 02/28/2019
+
+RESULTS_FILE="results.txt"
+IP_ADDR_DEFAULT="127.0.0.1"
+IP_ADDR=''
+
+if [ $# -eq 0 ]; then
+    read -p "Target IP/subnet [$IP_ADDR_DEFAULT]: " IP_ADDR
+    IP_ADDR=${IP_ADDR:=$IP_ADDR_DEFAULT}
+elif [ $# -eq 1 ]; then
+    IP_ADDR=$1
+else
+    printf "Error: Must specify IP address or subnet"
+    printf "Usage: ./scan.sh or ./scan.sh <IP_ADDR>"
     exit 1
 fi
 
-RESULTS_FILE = nmap-results.txt
+printf "Scanning $IP_ADDR\n" > $RESULTS_FILE
+printf "\n=== ACK Scan ===" >> $RESULTS_FILE
+nmap -n -sA $IP_ADDR >> $RESULTS_FILE
 
-echo '=== ACK Scan ==='
-nmap -sA $1 >> $RESULTS_FILE 2>&1
+printf "\n=== SYN Scan ===" >> $RESULTS_FILE
+nmap -n -sS $IP_ADDR >> $RESULTS_FILE
 
-echo '=== SYN Scan ==='
-nmap -sS $1 >> $RESULTS_FILE 2>&1
+printf "\n=== XMAS Scan ===" >> $RESULTS_FILE
+nmap -n -sX $IP_ADDR >> $RESULTS_FILE
 
-echo '=== XMAS Scan ==='
-nmap -sX $1 >> $RESULTS_FILE 2>&1
+printf "\n=== Version Scan ===" >> $RESULTS_FILE
+nmap -n -sV $IP_ADDR >> $RESULTS_FILE
 
-echo '=== Version Scan ==='
-nmap -sV $1 >> $RESULTS_FILE 2>&1
+printf "\n=== OS Scan ===" >> $RESULTS_FILE
+nmap -n -O $IP_ADDR >> $RESULTS_FILE
 
-echo '=== OS Scan ==='
-nmap -O $1 >> $RESULTS_FILE 2>&1
+printf "\nResults file: $RESULTS_FILE\n"
